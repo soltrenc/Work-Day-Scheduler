ad03bfc44dc308bcf4f0b3deb55f147ba52e963c
Chris Soltren <csoltren1@yahoo.com>
2020-02-03 14:07:05 -0500

    Finally did it

--- script.js
+++ script.js
@@ -7,23 +7,37 @@ var hourBlock = $(".container");
 var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
 for (i = 0; i < hours.length; i++) {
+    var currentHour = parseInt(moment().format("H"));
+    var plannerHour = parseInt(moment(hours[i], "ha").format("H"));
+    var color;
+    if (plannerHour < currentHour) {
+        color = "grey";
+    }
+    else if (plannerHour === currentHour) {
+        color = "red";
+    }
+    else {
+        color = "green";
+    }
+
     var time = $('<div>').addClass("row time-block").attr("id", "row" + i);
     $(".container").append(time);
     var targetTime = $("#row" + i);
     var timeCol = $("<div>").addClass("col-md-1 hour").text(hours[i]);
     $("#row" + i).append(timeCol);
-    var eventCol = $("<textarea>").addClass("col-md-10").text("Add your to-do here");
+    var eventCol = $("<textarea>").addClass("col-md-10" + " " + color).text(localStorage.getItem(hours[i]));
     $("#row" + i).append(eventCol);
     var savebuttonCol = $("<button>").addClass("col-md-1 saveBtn");
     $("#row" + i).append(savebuttonCol);
 }
 
 $('.saveBtn').click(function () {
-    console.log('in save btn click')
-    var textEvent = $(this).prev().html();
-    localStorage.setItem('calendar', textEvent);
-    console.log(textEvent);
+    var textEvent = $(this).prev().val();
+    var time = $(this).prev().prev().text();
+    localStorage.setItem(time, textEvent);
 });
+
+
 
 
 
